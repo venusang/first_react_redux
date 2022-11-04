a91e5dc267edd41218c9f1e4ce7c9c78bf352232
Venus Ang <venus@users.noreply.github.com>
2022-11-03 20:17:45 -0700

    added form function and form field to UI

--- src/Counter.js
+++ src/Counter.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './features/counterSlice'
+import { decrement, increment, incrementByAmount } from './features/counterSlice'
 
 function Counter() {
   const count = useSelector((state) => state.value);
   const dispatch = useDispatch();
 
+  //handleSubmit was changed - name is byAmountSubmit in lesson
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(incrementByAmount(Number(input)));
+  }
+  const [input, setInput] = useState(0);
+
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={() => dispatch(increment())} >
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
         Increment
       </button>
-      <button onClick={() => dispatch(decrement())} >
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
         Decrement
       </button>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="number" onChange={(e) => setInput(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
