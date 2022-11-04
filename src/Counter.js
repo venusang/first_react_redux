c41be78fa69a06bdce2d5ca504999309c2cf4e0e
Venus Ang <venus@users.noreply.github.com>
2022-11-03 20:05:33 -0700

    set up Counter with imports and UI bits

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,22 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './features/counterSlice'
+
 function Counter() {
-  return 'Counter stuff';
+  const count = useSelector((state) => state.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment())} >
+        Increment
+      </button>
+      <button onClick={() => dispatch(decrement())} >
+        Decrement
+      </button>
+    </div>
+  );
 }
 
 export default Counter;
